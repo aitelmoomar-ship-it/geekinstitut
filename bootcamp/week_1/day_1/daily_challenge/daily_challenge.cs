9f834518a7564a8dd0842e1cc11bf296fb24cfc3
omar <aitel.mo.omar@gmail.com>
2025-11-26 18:09:20 +0100

    e

--- bootcamp/week_1/day_1/daily_challenge/daily_challenge.cs
+++ bootcamp/week_1/day_1/daily_challenge/daily_challenge.cs
@@ -1,47 +1,47 @@
-//challange 1:
-using System;
-using System.Text;
+//                  //challange 1:
+// using System;
+// using System.Text;
 
-    Console.Write("Enter a number: ");
-    string input = Console.ReadLine();
-    int number = int.Parse(input);
+//     Console.Write("Enter a number: ");
+//     string input = Console.ReadLine();
+//     int number = int.Parse(input);
 
-    Console.Write("Enter a length: ");
-    string input2 = Console.ReadLine();
-    int length = int.Parse(input2);
+//     Console.Write("Enter a length: ");
+//     string input2 = Console.ReadLine();
+//     int length = int.Parse(input2);
 
-    List<int> results1 = new List<int>();
+//     List<int> results1 = new List<int>();
 
-    for (int i = 1; i <= length; i++)
-    {
-        int result = i*number;
-        results1.Add(result);
-    }
-    {
-        Console.WriteLine(string.Join(results1));
-    }
+//     for (int i = 1; i <= length; i++)
+//     {
+//         int result = i*number;
+//         results1.Add(result);
+//     }
+//     {
+//         Console.WriteLine(string.Join(results1));
+//     }
 
-//challange 2:
+//                  // challange 2:
 
-Console.Write("Enter a string: ");
-string inputStr= Console.ReadLine();
-StringBuilder resultBuilder = new StringBuilder();
+// Console.Write("Enter a string: ");
+// string inputStr= Console.ReadLine();
+// StringBuilder resultBuilder = new StringBuilder();
 
-char lastChar = '\0'; 
+// char lastChar = '\0'; 
 
-for (int i = 0; i < inputString.Length; i++)
-{
-    char currentChar = inputStr[i];
+// for (int i = 0; i < inputString.Length; i++)
+// {
+//     char currentChar = inputStr[i];
 
-    if (currentChar != lastChar)
-    {
-        resultBuilder.Append(currentChar);
+//     if (currentChar != lastChar)
+//     {
+//         resultBuilder.Append(currentChar);
 
-        lastChar = currentChar;
-    }
-}
+//         lastChar = currentChar;
+//     }
+// }
 
-Console.WriteLine($"Original: {inputStr}");
-Console.WriteLine($"Result: {resultBuilder.ToString()}");
+// Console.WriteLine($"Original: {inputStr}");
+// Console.WriteLine($"Result: {resultBuilder.ToString()}");
 
 
---
+++ bootcamp/week_1/day_1/exercises_xp_gold/exercises_xp_gold.cs
@@ -0,0 +1,40 @@
+// --- Exercice1 FizzBuzz ---
+
+Console.Write("Entrez un nombre entre 1 et 100 : ");
+string input = Console.ReadLine();
+if (int.TryParse(input, out int number))
+{
+   
+    if (number % 15 == 0)
+    {
+        Console.WriteLine("FizzBuzz");
+    }
+    else if (number % 3 == 0)
+    {
+        Console.WriteLine("Fizz");
+    }
+    else if (number % 5 == 0)
+    {
+        Console.WriteLine("Buzz");
+    }
+    else
+    {
+        Console.WriteLine(number);
+    }
+}
+
+// --- Exercice2 FizzBuzz ---
+
+// --- Exercice Triangle ---
+
+int height = 5;
+
+for (int table = 1; table<= height; table++)
+{
+    string stars = "";
+    for (int star = 1; star <= table; star++)
+    {
+        stars += "*";
+    }
+    Console.WriteLine(stars);
+}
