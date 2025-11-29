c0e2928b8ef0f2ec8a0758c5a5927c1190fbb23d
omar <aitel.mo.omar@gmail.com>
2025-11-29 13:24:46 +0100

    exercice2

--- bootcamp/week_1/day_3/exercice_xp.cs
+++ bootcamp/week_1/day_3/exercice_xp.cs
@@ -1,11 +1,14 @@
-//exercicees 1 :
-List<string> keys = new List<string> { "Ten", "Twenty", "Thirty" };
-List<int> values = new List<int> { 10, 20, 30 };
-// 1. Create the empty Dictionary
-Dictionary<string, int> myDict = new Dictionary<string, int>();
+ //exercicees 1 :
+// List<string> keys = new List<string> { "Ten", "Twenty", "Thirty" };
+// List<int> values = new List<int> { 10, 20, 30 };
+// var diction = keys.Zip(values, (k, v)  => new {Key=k, Value=v}).ToDictionary(item => item.Key, item => item.Value);
 
-// 2. Loop through the lists and add items
-for (int i = 0; i < keys.Count; i++)
-{
-    myDict.Add(keys[i], values[i]);
-}
+// Console.WriteLine("{");
+// foreach (var t in diction)
+// {
+//     Console.Write($"\"{t.Key}\" , {t.Value} ");
+// }
+// Console.WriteLine("}");
+
+// exercice 2 :
+
